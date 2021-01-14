41f81a086d73ba6ee3a398ea7ded23954fb8765e
Ivan Chupriyanov <Aerocoolbye@yandex.ru>
2021-01-14 07:21:53 +0700

    lesson4

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -16,47 +16,11 @@ namespace Snake
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
 
-            Point p3 = new Point(2, 7, '&');
-            p3.Draw();
+            HorizontalLine line = new HorizontalLine(5, 10, 8, '+');
+            line.Drow();
 
-            Point p4 = new Point(9, 6, '$');
-            p4.Draw();
-
-            List<int> numList = new List<int>();
-            numList.Add(0);
-            numList.Add(1);
-            numList.Add(2);
-
-            int x = numList[0];
-            int y = numList[1];
-            int z = numList[2];
-
-            foreach(int i in numList)
-            {
-                Console.WriteLine(i);
-            }
-
-            numList.RemoveAt(0);
-
-            List<int> numberList = new List<int>();
-            numberList.Add(5);
-            numberList.Add(7);
-            numberList.Add(3);
-
-            int q = numberList[0];
-            int w = numberList[1];
-            int e = numberList[2];
-
-            foreach (int i in numberList)
-            {
-                Console.WriteLine(i);
-            }
-
-            List<Point> pList = new List<Point>();
-            pList.Add(p1);
-            pList.Add(p2);
-            pList.Add(p3);
-            pList.Add(p4);
+            VerticalLine l = new VerticalLine(2, 15, 18, '$');
+            l.Drow();
 
             Console.ReadLine();
         }
